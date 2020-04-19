3152c45bc94412cdd222026abf4552d5019a5406
Pakaporn <43027162+yykpkp@users.noreply.github.com>
2020-04-19 23:23:43 +0700

    change env

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/purinutmbp/Desktop/development/IT-booking-system/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
