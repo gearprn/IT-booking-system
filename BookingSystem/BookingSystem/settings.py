9500ece2f3d0f193cc788ad61e49b175fa608b53
gearprn <gearprn@gmail.com>
2020-04-23 21:56:54 +0700

    update: conect psycopg database

--- BookingSystem/BookingSystem/settings.py
+++ BookingSystem/BookingSystem/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'BookingSystem.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], # add global templates folder
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -73,10 +73,15 @@ WSGI_APPLICATION = 'BookingSystem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# add database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'booking_system',
+        'USER': 'postgres',
+        'PASSWORD': '159723',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -118,3 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# add static folder
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
+# add media forder to upload media file
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
