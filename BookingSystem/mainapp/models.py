b2de85c9aa1f81fd6752c41f7ac7502e083548f8
Pakaporn <43027162+yykpkp@users.noreply.github.com>
2020-04-24 20:08:12 +0700

    add: room models

--- BookingSystem/mainapp/models.py
+++ BookingSystem/mainapp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+
+    availableStatus = (
+        ('AVAILABLE', 'Available'),
+        ('UNDER MAINTENANCE', 'Under Maintenance')
+    )
+
+    size = models.IntegerField()
+    photos = models.ImageField(upload_to='RoomPhotos')
+    roomType = models.ForeignKey('mainapp.RoomType', on_delete=models.CASCADE)
+
+class RoomType(models.Model):
+    typeTitle = models.CharField(max_length=255)
+
+
+
+class Facility(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+class RoomType_Facility(models.Model):
+    roomType = models.ForeignKey('mainapp.RoomType', on_delete=models.CASCADE)
+    facility = models.ForeignKey('mainapp.Facility', on_delete=models.CASCADE)
+    amount = models.IntegerField()
